755035109fb109dff6e963e33095ef34393d6769
= <Hoangvantien2301@gmail.com>
2019-07-03 10:08:17 +0900

    xu ly sroll

--- 1.js
+++ 1.js
@@ -1,4 +1,11 @@
 ﻿ $(function(){
+ 	vitricuaedu = $('#khoiedu').offset().top;
+ 	//console.log(vitricuaedu);
+
+ 	$(".xuong,.kn").click(function() {
+ 		/* Act on the event */
+ 		$('body,html').animate({scrollTop:vitricuaedu},600)
+ 	});
  
 })  
  
