241b6492f6ad505818f854c62ccb67574f9a01a2
= <Hoangvantien2301@gmail.com>
2019-07-03 14:13:31 +0900

    xu ly cmt facebook

--- 1.js
+++ 1.js
@@ -1,11 +1,16 @@
 ﻿ $(function(){
  	vitricuaedu = $('#khoiedu').offset().top;
+ 	vitricuacmt = $('#khoicmt').offset().top;
  	//console.log(vitricuaedu);
 
  	$(".xuong,.kn").click(function() {
  		/* Act on the event */
  		$('body,html').animate({scrollTop:vitricuaedu},600)
  	});
- 
+	
+	 $(".cmt").click(function() {
+ 		/* Act on the event */
+ 		$('body,html').animate({scrollTop:vitricuacmt},600)
+ 	});
 })  
  
