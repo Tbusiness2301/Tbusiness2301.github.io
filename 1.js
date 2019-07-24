c5abd9d0c23f95f7cd7ab2274cf67d31f1e8d780
= <Hoangvantien2301@gmail.com>
2019-07-24 16:46:10 +0900

    update ver2

--- 1.js
+++ 1.js
@@ -11,6 +11,25 @@
 
  	});
 
+ 	//hieu ung cho menu
+ 	Addservices = $('#InfoServices').offset().top;
+ 	$('.services').click(function() {
+ 		/* Act on the event */
+ 		$('body,html').animate({scrollTop:Addservices},700);
+ 	});
+
+
+ 	AddCaseStudies = $('#casestudies').offset().top;
+ 	$('.casestudies').click(function() {
+ 		/* Act on the event */
+ 		$('body,html').animate({scrollTop:AddCaseStudies},700);
+ 	});
+
+ 	AddNews = $('#News').offset().top;
+ 	$('.news').click(function() {
+ 		/* Act on the event */
+ 		$('body,html').animate({scrollTop:AddNews},700);
+ 	});
  	//hieu ung cuon chuot
  	new WOW().init();
 })  
